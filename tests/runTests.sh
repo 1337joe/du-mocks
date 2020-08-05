4bad88789cf9933e21f837341640d1ea34854693
Joe Rogers <1337joe@gmail.com>
2020-08-05 23:26:00 +0200

    make runTests find tests instead of listing them

--- tests/runTests.sh
+++ tests/runTests.sh
@@ -4,7 +4,5 @@ cd "$(dirname "$0")"
 rm -rf results
 mkdir results
 
-./TestMockElement.lua -o junit -n results/TestMockElement.xml
-./TestMockContainerUnit.lua -o junit -n results/TestMockContainerUnit.xml
-./TestMockDatabankUnit.lua -o junit -n results/TestMockDatabankUnit.xml
+find . -name Test\*.lua -exec ./{} -o junit -n results/{}.xml \;
 
